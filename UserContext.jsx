e62ebdcfe05a1e9751df53d8b8c252cafd7483ea
ParthDua0 <115578200+ParthDua0@users.noreply.github.com>
2026-01-10 13:46:40 +0530

    implemented basic useState

---
+++ UserContext.jsx
@@ -0,0 +1,15 @@
+import { Children, createContext, useContext,useState } from "react";
+const UserContext=createContext()
+
+const UserProvider = ({children}) =>{
+    const [user, setUser] = useState({name: "John Doe"})
+
+    const updateUser = (newName) => {
+        setUser({name: newName})
+    }
+
+    return <UserContext.Provider value ={{user, updateUser}}>
+        {children}
+    </UserContext.Provider>
+}
+export {UserContext,UserProvider};
